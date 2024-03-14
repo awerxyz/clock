6999240a2e4084ea00bf58e53dc08598f6f36170
rucen <rucen.gss@gmail.com>
2024-03-14 20:22:47 +0100

    functional on pc

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let second = 0;
 let minute = 0;
 let hour = 0;
@@ -14,5 +15,24 @@ setInterval(
         document.getElementById("sec-hand-holder").style.transform = "rotate(" + second + "deg)";
         document.getElementById("min-hand-holder").style.transform = "rotate(" + minute + "deg)";
         document.getElementById("hour-hand-holder").style.transform = "rotate(" + hour + "deg)";
-    }, 1000
+
+        let secNumbers = document.getElementsByClassName("sec-number");
+        for (let i = 0; i < secNumbers.length; i++) {
+            secNumbers[i].style.transform = "rotate(" + -(second + -90) + "deg)";
+            secNumbers[i].textContent = date.getSeconds();
+        }
+
+        let minNumbers = document.getElementsByClassName("min-number");
+        for (let i = 0; i < minNumbers.length; i++) {
+            minNumbers[i].style.transform = "rotate(" + -(minute + -90) + "deg)";
+            minNumbers[i].textContent = date.getMinutes();
+        }
+
+        let hourNumbers = document.getElementsByClassName("hour-number");
+        for (let i = 0; i < hourNumbers.length; i++) {
+            hourNumbers[i].style.transform = "rotate(" + -(hour + -90) + "deg)";
+            hourNumbers[i].textContent = date.getHours();
+        }
+
+    },1000
 )
